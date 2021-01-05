313651392f2b8d3b74d112b0666d4653c06f4080
Rémi Desgrange <remi.desgrange@camptocamp.com>
2021-01-05 16:12:03 +0100

    First version of the model

--- app/drealcorsereports/models/reports.py
+++ app/drealcorsereports/models/reports.py
@@ -1,20 +1,37 @@
 # coding=utf-8
-from sqlalchemy import (
-    Column,
-    DateTime,
-    Integer,
-    String,
-)
+from sqlalchemy import Column, DateTime, Integer, String, ForeignKey
 from drealcorsereports.models.meta import Base
+from sqlalchemy.dialects.postgresql import UUID, JSON
+from uuid import uuid4
+from sqlalchemy.orm import relationship
 
 SCHEMA = "reports"
+
+
+class Template(Base):
+    __tablename__ = "template"
+    __table_args__ = {"schema": SCHEMA}
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    # postgres offer JSON and JSONB. Binary is more powerful for querying but
+    # since we always want all the JSON and not some part. JSON is more
+    # convinent (and insert is faster).
+    # Schema store the json schema that will be interpret by the frontend.
+    schema = Column(JSON, nullable=False)
+    layer_id = Column(String, nullable=False)
 
 
 class Report(Base):
     __tablename__ = "report"
     __table_args__ = {"schema": SCHEMA}
 
-    id = Column(Integer, primary_key=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     feature_id = Column(String, nullable=False)
+    template_id = Column(UUID(as_uuid=True), ForeignKey("template.id"), nullable=False)
+    template = relationship("Template")
+    # response of the form. based on the template json schema
+    response = Column(JSON, nullable=False)
     updated_at = Column(DateTime(timezone=True), nullable=False)
+    # Since user comes from HTTP header (handled by georchestra security-proxy)
+    # we don't have any User class in this app.
     updated_by = Column(String, nullable=False)
