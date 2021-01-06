df6e229d79d8b125b138a2c72f68c591fac4283a
Tobias Kohr <tobias.kohr@camptocamp.com>
2021-01-06 17:15:20 +0100

    calls API in dev via proxy

--- app/drealcorsereports/static/admin/src/App.js
+++ app/drealcorsereports/static/admin/src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  fetch('/api')
+    .then(response => response.json())
+    .then(data => console.log(data));
+
   return (
     <div className="App">
       <header className="App-header">
--- app/drealcorsereports/views.py
+++ app/drealcorsereports/views.py
@@ -1,6 +1,7 @@
 """ Cornice services.
 """
 from cornice import Service
+from pyramid.view import (view_config)
 
 
 hello = Service(name="hello", path="/", description="Simplest app")
@@ -11,3 +12,11 @@ def get_info(request):
     """Returns Hello in JSON."""
     del request
     return {"Hello": "World"}
+
+class MockApi:
+  def __init__(self, request):
+    self.request = request
+
+  @view_config(route_name='mock_api', renderer='json')
+  def hello(self):
+      return {'Hello': 'API'}
