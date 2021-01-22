9ab89777f6a556cd9b9a18cd005225743d24f58e
Tobias Kohr <tobias.kohr@camptocamp.com>
2021-01-22 14:13:04 +0100

    fix import and format (rebase)

--- app/drealcorsereports/views.py
+++ app/drealcorsereports/views.py
@@ -1,6 +1,7 @@
 """ Cornice services.
 """
 from cornice import Service
+from pyramid.view import view_config
 
 
 hello = Service(name="hello", path="/api", description="Simplest app")
@@ -20,42 +21,32 @@ class MockApi:
     @view_config(route_name="get_report_models", renderer="json")
     def getAll(self):
         return [
-            {
-                "id": "1",
-                "title": "TestTitle",
-                "layer": "TestLayer"
-            },
-            {
-                "id": "2",
-                "title": "TestTitle2",
-                "layer": "TestLayer2"
-            }
+            {"id": "1", "title": "TestTitle", "layer": "TestLayer"},
+            {"id": "2", "title": "TestTitle2", "layer": "TestLayer2"},
         ]
 
     @view_config(route_name="get_report_model_by_id1", renderer="json")
     def getOne(self):
         return {
-                "id": "1",
-                "title": "TestTitle",
-                "layer": "TestLayer",
-                "properties":
-                    [
-                        {"type": "string", "name": "Name", "required": True},
-                        {"type": "string", "name": "Description", "required": False},
-                        {"type": "number", "name": "Nr", "required": False}
-                    ]
-            }
+            "id": "1",
+            "title": "TestTitle",
+            "layer": "TestLayer",
+            "properties": [
+                {"type": "string", "name": "Name", "required": True},
+                {"type": "string", "name": "Description", "required": False},
+                {"type": "number", "name": "Nr", "required": False},
+            ],
+        }
 
     @view_config(route_name="get_report_model_by_id2", renderer="json")
     def getTwo(self):
         return {
-                "id": "2",
-                "title": "TestTitle2",
-                "layer": "TestLayer2",
-                "properties":
-                    [
-                        {"type": "string", "name": "Title", "required": True},
-                        {"type": "number", "name": "Length", "required": True},
-                        {"type": "string", "name": "Summary", "required": True}
-                    ]
-            }
+            "id": "2",
+            "title": "TestTitle2",
+            "layer": "TestLayer2",
+            "properties": [
+                {"type": "string", "name": "Title", "required": True},
+                {"type": "number", "name": "Length", "required": True},
+                {"type": "string", "name": "Summary", "required": True},
+            ],
+        }
