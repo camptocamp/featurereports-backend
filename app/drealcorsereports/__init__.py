99b49c4261c2e1418fdb4629d9843c491c67e7b5
Tobias Kohr <tobias.kohr@camptocamp.com>
2021-01-27 15:25:03 +0100

    removes mock API

--- app/drealcorsereports/__init__.py
+++ app/drealcorsereports/__init__.py
@@ -10,9 +10,6 @@ def main(global_config, **settings):
     config.include("drealcorsereports.models.includeme")
     config.include("drealcorsereports.security")
     config.include("cornice")
-    config.add_route("get_report_models", "/admin/report_models")
-    config.add_route("get_report_model_by_id1", "/admin/report_models/1")
-    config.add_route("get_report_model_by_id2", "/admin/report_models/2")
     config.add_static_view("admin", "/opt/drealcorsereports/static/admin/build")
 
     config.scan("drealcorsereports.views")
