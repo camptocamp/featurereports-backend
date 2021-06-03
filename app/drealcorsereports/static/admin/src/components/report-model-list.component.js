a80551433728c8e819e62cafc8cfa21529a614db
isanchez-c2c <ivanne.sanchez@camptocamp.com>
2021-06-03 09:56:38 +0200

    admin front : improve report model list UI

--- app/drealcorsereports/static/admin/src/components/report-model-list.component.js
+++ app/drealcorsereports/static/admin/src/components/report-model-list.component.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
-import { FaPen } from 'react-icons/fa';
+import './report-model-list.component.css';
 
 export default class ReportModelList extends Component {
   render() {
@@ -11,16 +11,29 @@ export default class ReportModelList extends Component {
       const dateStringFR = date.toLocaleString('fr-FR');
       return dateStringFR;
     };
+    const formatLink = (cell, row) => {
+      return <button
+              type="button"
+              class="btn btn-link"
+              onClick={() => this.props.editReportModel(row)}
+              text="cell"
+            >
+              {cell}
+            </button>;
+    };
     const columns = [
       {
         dataField: 'title',
         text: 'Titre',
-        sort: true,
+        sort: true
       },
       {
         dataField: 'name',
         text: 'Nom',
         sort: true,
+        formatter: (cell, row) => {
+          return formatLink(cell, row);
+        },
       },
       {
         dataField: 'layer_id',
@@ -52,21 +65,7 @@ export default class ReportModelList extends Component {
         dataField: 'updated_by',
         text: 'Mise à jour par',
         sort: true,
-      },
-      {
-        dataField: 'edit_button',
-        text: '',
-        isDummyField: true,
-        align: 'center',
-        formatter: (cell, row) => (
-          <button
-            onClick={() => this.props.editReportModel(row)}
-            className="btn btn-secondary"
-          >
-            <FaPen />
-          </button>
-        ),
-      },
+      }
     ];
 
     return (
